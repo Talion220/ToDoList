114d2f647454c6d78e65dc6c53bcb96dcfd73e8a
Владислав Харченко <vladislav.harchenko2017@yandex.ru>
2024-01-07 18:28:18 +0700

    add activity added

--- app/src/main/java/com/bignerdranch/android/todolist/AddActivity.kt
+++ app/src/main/java/com/bignerdranch/android/todolist/AddActivity.kt
@@ -2,33 +2,52 @@ package com.bignerdranch.android.todolist
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.todolist.databinding.ActivityAddBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class AddActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddBinding
-
+    private lateinit var taskDao: TaskDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_add)
 
-        // Доступ к компонентам макета через binding
-        val editTextDescription = binding.editTextDescription
-        val radioGroupPriority = binding.radioGroupPriority
+        taskDao = TaskListApplication.database.taskDao()
         val buttonAdd = binding.buttonAdd
-
-        // Ваш код для работы с editTextDescription, radioGroupPriority и buttonAdd
-
 
         buttonAdd.setOnClickListener {
 
-            val intent = Intent(this@AddActivity, ListActivity::class.java)
+            val description = binding.editTextDescription.text.toString()
+            val priority = getSelectedPriority()
 
-            // Запускаем AddActivity
-            startActivity(intent)
+            GlobalScope.launch(Dispatchers.IO) {
+                taskDao.insertTask(Task(description = description, priority = priority))
+            }
+
+            finish()
+        }
+
+    }
+    private fun getSelectedPriority(): Int {
+        val priorityRadioGroup: RadioGroup = findViewById(R.id.radioGroupPriority)
+
+        val selectedRadioButton: RadioButton? = findViewById(priorityRadioGroup.checkedRadioButtonId)
+
+        return when (selectedRadioButton?.text.toString().toLowerCase()) {
+            "high" -> 1 // Пример: высокий приоритет
+            "medium" -> 2 // Пример: средний приоритет
+            "low" -> 3 // Пример: низкий приоритет
+            else -> 0 // По умолчанию, если ничего не выбрано
         }
     }
 }
